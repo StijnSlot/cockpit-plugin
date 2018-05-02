e2a23f1f273ae020ba083a05e4aaabcc01849c11
Martin <mcekodhima@gmail.com>
2018-05-02 15:20:07 +0200

    Created new branch

--- src/main/resources/plugin-webapp/sample-plugin/app/plugin.js
+++ src/main/resources/plugin-webapp/sample-plugin/app/plugin.js
@@ -1,29 +1,9 @@
-define(['angular'], function(angular) {
+/*
+ * Loads all the modules of our plugin.
+ */
+'use strict';
 
-  var DashboardController = ["$scope", "$http", "Uri", function($scope, $http, Uri) {
+var angular = require('angular'),
+    diagramModule = require('./diagram/main');
 
-    $http.get(Uri.appUri("plugin://sample-plugin/:engine/process-instance"))
-      .success(function(data) {
-        $scope.processInstanceCounts = data;
-      });
-  }];
-
-  var Configuration = ['ViewsProvider', function(ViewsProvider) {
-
-    ViewsProvider.registerDefaultView('cockpit.dashboard', {
-      id: 'process-definitions',
-      label: 'Deployed Processes',
-      url: 'plugin://sample-plugin/static/app/dashboard.html',
-      controller: DashboardController,
-
-      // make sure we have a higher priority than the default plugin
-      priority: 12
-    });
-  }];
-
-  var ngModule = angular.module('cockpit.plugin.sample-plugin', []);
-
-  ngModule.config(Configuration);
-
-  return ngModule;
-});
+module.exports = angular.module('cockpit.plugin.sample-plugin', [diagramModule.name]);
