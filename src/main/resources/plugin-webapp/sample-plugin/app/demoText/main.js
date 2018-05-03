01adf36c688bd072fe21f0cc4743f01c26260a6d
timhuebener <tim.huebener@gmail.com>
2018-05-03 15:52:03 +0200

    trying stuff

--- src/main/resources/plugin-webapp/sample-plugin/app/demoText/main.js
+++ src/main/resources/plugin-webapp/sample-plugin/app/demoText/main.js
@@ -1,15 +1,18 @@
 'use strict'
+
+// var instanceCount = require('src\main\resources\plugin-webapp\sample-plugin\app\demoText\brain.js');
+
 define(['angular'], function(angular) {
 
     var Configuration = [ 'ViewsProvider', function(ViewsProvider) {
         ViewsProvider.registerDefaultView('cockpit.processDefinition.diagram.plugin', {
-            id: 'runtime',
+            id: 'runtime-demoText',
             priority: 20,
             label: 'Runtime',
             overlay: [
                 'control', 'processData', 'pageData', 'processDiagram',
                 function(control, processData, pageData, processDiagram) {
-                    console.log("hello");
+                    console.log("test demoText");
                 }]
         });
     }];
