517f306351dae840c9f247a256cfa024e5ec9686
Lukas Ant <lukas.ant@hotmail.com>
2018-05-16 11:09:15 +0200

    Current time working

--- src/main/resources/plugin-webapp/centaur/app/diagram/duration/main.js
+++ src/main/resources/plugin-webapp/centaur/app/diagram/duration/main.js
@@ -37,69 +37,96 @@ define(['angular'], function(angular) {
                         return millis / 60000;
                     }
 
+                    function getStartTime(){
+                        console.log("getStartTime");
+                        $http.get(Uri.appUri("plugin://centaur/:engine/instance-start-time"))
+                                .success(function(data) {
+                                    $scope.instanceStartTime = data;
+                                    console.log($scope.instanceStartTime);
+                                });
+                        console.log("StartTime received");
+                        console.log($scope.instanceStartTime);
+                        return $scope.instanceStartTime;
+                    }
+
                     $http.get(Uri.appUri("plugin://centaur/:engine/process-activity?" +
                                         "procDefId=" + procDefId))
                         .success(function(data) {
                             $scope.processActivityStatistics = data;
 
+                            $http.get(Uri.appUri("plugin://centaur/:engine/instance-start-time"))
+                                .success(function(data) {
+                                    $scope.instanceStartTime = data;
+                                    console.log($scope.instanceStartTime);
 
+                                    console.log("StartTime received");
 
-                            //console.log($scope.processActivityStatistics);
-                            //console.log('Here comes the duration data');
+                                    //console.log($scope.processActivityStatistics);
 
-                            elementRegistry.forEach(function(shape) { 
-                                var element = processDiagram.bpmnElements[shape.businessObject.id];
-                                //console.log(element.id);
-        
-                                function addColorToId(elementId, duration) {
-                                    var $overlayHtml =
-                                            $(duration)
-                                            .css({
-                                                width: shape.width,
-                                                height: shape.height
-                                            });
-            
-                                        overlays.add(elementId, {
-                                            position: {
-                                            top: -30,
-                                            left: -30
-                                            },
-                                            show: {
-                                              minZoom: -Infinity,
-                                              maxZoom: +Infinity
-                                            },
-                                            html: $overlayHtml
-                                        });
-                                }
-        
-                                for (var i = 0; i < $scope.processActivityStatistics.length; i++) {
-                                    if ($scope.processActivityStatistics[i].id == element.id) {
-                                        // console.log('Its the same');
-                                        // console.log($scope.processActivityStatistics[i].id);
-                                        // console.log(element.id);
-                                        var getAvgDuration = millisToMinutes($scope.processActivityStatistics[i].avgDuration);
-                                        var getMinDuration = millisToMinutes($scope.processActivityStatistics[i].minDuration);
-                                        var getMaxDuration = millisToMinutes($scope.processActivityStatistics[i].maxDuration);
-                                        if (getAvgDuration != null && getMinDuration != null && getMaxDuration != null && getAvgDuration != '0') {
-                                            var htmlText2 = getAvgDuration.toString();
-                                            var htmlText3 = getMinDuration.toString();
-                                            var htmlText4 = getMaxDuration.toString();
-                                            var htmlText = htmlText1 + 'Avg:' + htmlText2 + ' min <br>' + 'Min:' +  htmlText3 + ' min <br>' + 'Max:' +  htmlText4 + ' min' + htmlText5;
-                                            addColorToId(element.id, htmlText);
+                                    elementRegistry.forEach(function(shape) { 
+                                        var element = processDiagram.bpmnElements[shape.businessObject.id];
+                                        //console.log(element.id);
+                
+                                        function addColorToId(elementId, duration) {
+                                            var $overlayHtml =
+                                                    $(duration)
+                                                    .css({
+                                                        width: shape.width,
+                                                        height: shape.height
+                                                    });
+                    
+                                                overlays.add(elementId, {
+                                                    position: {
+                                                    top: -30,
+                                                    left: -30
+                                                    },
+                                                    show: {
+                                                    minZoom: -Infinity,
+                                                    maxZoom: +Infinity
+                                                    },
+                                                    html: $overlayHtml
+                                                });
                                         }
-                                        break;
-                                    }
-                                }                        
-                            });
+                
+                                        for (var i = 0; i < $scope.processActivityStatistics.length; i++) {
+                                            if ($scope.processActivityStatistics[i].id == element.id) {
+                                                // console.log('Its the same');
+                                                // console.log($scope.processActivityStatistics[i].id);
+                                                console.log(element.id);
+                                                var getAvgDuration = millisToMinutes($scope.processActivityStatistics[i].avgDuration);
+                                                var getMinDuration = millisToMinutes($scope.processActivityStatistics[i].minDuration);
+                                                var getMaxDuration = millisToMinutes($scope.processActivityStatistics[i].maxDuration);
 
-                            
-                        });
+                                                var getCurrentTime = 'not found';
 
-                    
-                    
+                                                for (var j = 0; j < $scope.instanceStartTime.length; j++) {
+                                                    if ($scope.instanceStartTime[j].activityId == element.id) {
+                                                        var getCurrentTime = $scope.instanceStartTime[j].startTime;
+                                                        var getCurrentTimeMS = Date.parse(getCurrentTime);
+                                                        var getComputerTime = new Date();
+                                                        var getComputerTimeMS = getComputerTime.getTime();
+                                                        var getActualTime = getComputerTimeMS - getCurrentTimeMS;
+                                                        var getActualTime = millisToMinutes(getActualTime);
+                                                        break;
+                                                    }
+                                                }
 
+                                                console.log(getCurrentTime);
 
-                    
+                                                if (getAvgDuration != null && getMinDuration != null && getMaxDuration != null && getAvgDuration != '0') {
+                                                    var htmlText2 = getAvgDuration.toString();
+                                                    var htmlText3 = getMinDuration.toString();
+                                                    var htmlText4 = getMaxDuration.toString();
+                                                    var htmlText4222 = getActualTime.toString();
+                                                    var htmlText = htmlText1 + 'Avg:' + htmlText2 + ' min <br>' + 'Min:' +  htmlText3 + ' min <br>' + 'Max:' +  htmlText4 + ' min <br>' + 'Cur:' + htmlText4222 + htmlText5;
+                                                    addColorToId(element.id, htmlText);
+                                                }
+                                                break;
+                                            }
+                                        }                        
+                                    });
+                                });     
+                        });  
                 }]
         });
     }];
