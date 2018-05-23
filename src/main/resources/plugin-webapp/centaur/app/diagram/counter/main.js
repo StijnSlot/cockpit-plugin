baebcf42f37c41602fd46e008e9839a1bb069c3f
timhuebener <tim.huebener@gmail.com>
2018-05-23 09:50:53 +0200

    Improved contracts

--- src/main/resources/plugin-webapp/centaur/app/diagram/counter/main.js
+++ src/main/resources/plugin-webapp/centaur/app/diagram/counter/main.js
@@ -28,9 +28,8 @@ define(['angular'], function(angular) {
 
           /**
            * Converts variable to String.
-           * @param   Number  toConvert   Variable to be converted.
-           * @return  String              String representation of toConvert
-           * TODO: make into service
+           * @param  {Number} toConvert Variable to be converted.
+           * @return {String}           String representation of toConvert
            */
           function converToString(toConvert) {
             return toConvert.toString();
@@ -38,9 +37,9 @@ define(['angular'], function(angular) {
 
           /**
            * Adds text to specified diagram element.
-           * @param   Number  elementId   ID of diagram element
-           * @param   Number  text        The text to be displayed
-           * @param   Object  shape       Shape of the element
+           * @param   {Number}  elementId   ID of diagram element
+           * @param   {Number}  text        The text to be displayed
+           * @param   {Object}  shape       Shape of the element
            * TODO: make into service
            */
           function addTextToId(elementId, text, shape) {
@@ -77,12 +76,12 @@ define(['angular'], function(angular) {
            * so that the duration varables are displayed next to the
            * process diagram element.
            *
-           * @param   Number  minDuration   minimal duration of process
-           * @param   Number  avgDuration   average duration of process
-           * @param   Number  maxDuration   maximal duration of process
-           * @param   Number  curDuration   current duration of process
-           * @param   Number  elementID     ID of element
-           * @param   Object  shape         Shape of the element
+           * @param   {Number}  minDuration   minimal duration of process
+           * @param   {Number}  avgDuration   average duration of process
+           * @param   {Number}  maxDuration   maximal duration of process
+           * @param   {Number}  curDuration   current duration of process
+           * @param   {Number}  elementID     ID of element
+           * @param   {Object}  shape         Shape of the element
            */
           function composeHTML(minDuration, avgDuration, maxDuration, curDuration, elementID, shape) {
             if (avgDuration != null && minDuration != null && maxDuration != null && avgDuration != '0') {
@@ -105,8 +104,8 @@ define(['angular'], function(angular) {
            * the process activity and the instance start time.
            */
           //TODO: Get data from database
-          $scope.processActivityStatistics_temp = $http.get(Uri.appUri("plugin://centaur/:engine/process-activity?" + "procDefId=" + procDefId), {catch: false});
-          $scope.instanceStartTime_temp = $http.get(Uri.appUri("plugin://centaur/:engine/instance-start-time"), {catch: false});
+          // $scope.processActivityStatistics_temp = $http.get(Uri.appUri("plugin://centaur/:engine/process-activity?" + "procDefId=" + procDefId), {catch: false});
+          // $scope.instanceStartTime_temp = $http.get(Uri.appUri("plugin://centaur/:engine/instance-start-time"), {catch: false});
 
           /**
            * Waits until data is received from http.get request and
@@ -115,7 +114,7 @@ define(['angular'], function(angular) {
            * Database quersies take a relative long time. So we have to
            * wait until the data is retrieved before we can continue.
            *
-           * @param   Object  data   minimal duration of process
+           * @param   {Object}  data   minimal duration of process
            */
           $q.all([$scope.processActivityStatistics_temp, $scope.instanceStartTime_temp]).then(function(data) {
             $scope.processActivityStatistics = data[0]; //$scope.processActivityStatistics.data to access array with data from JSON object
@@ -125,7 +124,7 @@ define(['angular'], function(angular) {
              * Extracts data from JSON objects and calls composeHTML()
              * function to add the extracted to the diagram.
              *
-             * @param   Object  shape   shape of element
+             * @param   {Object}  shape   shape of element
              * TODO: refactor out into seperate function
              */
             elementRegistry.forEach(function(shape) {
