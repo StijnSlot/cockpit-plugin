e11693104ecb01c573af2be5f214e2700fd00306
xanderwessels <x.b.wessels@student.tue.nl>
2018-05-25 10:22:05 +0200

    fixed height and width

--- src/main/resources/plugin-webapp/centaur/app/diagram/bulletgraph/main.js
+++ src/main/resources/plugin-webapp/centaur/app/diagram/bulletgraph/main.js
@@ -369,9 +369,8 @@ define(['angular'], function (angular) {
                                                 }
                                             ];
                                             var container = d3.select('.bullet-chart-container').node().getBoundingClientRect();
-                                            var margin = { top: 5, right: 40, bottom: 20, left: 120 },
-                                                width = container.width - margin.left - margin.right,
-                                                height = 75 - margin.top - margin.bottom;
+                                            var width = 100;
+                                            var height = 40;
 
                                             var chart = d3.bullet(width, height)
                                                 .width(width)
@@ -381,61 +380,17 @@ define(['angular'], function (angular) {
                                                 .data(data)
                                                 .enter().append("svg")
                                                 .attr("class", "bullet")
-                                                .attr("width", "auto")
-                                                .attr("height", height + margin.top + margin.bottom)
+                                                .attr("width", width)
+                                                .attr("height", height)
                                                 .append("g")
-                                                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
                                                 .call(chart);
-
-                                            var title = svg.append("g")
-                                                .style("text-anchor", "end")
-                                                .attr("transform", "translate(-6," + height / 2 + ")");
-
-                                            title.append("text")
-                                                .attr("class", "title")
-                                                .text(function (d) { return d.title; });
-
-                                            title.append("text")
-                                                .attr("class", "subtitle")
-                                                .attr("dy", "1em")
-                                                .text(function (d) { return d.subtitle; });
-
-                                            d3.selectAll("button").on("click", function () {
-                                                svg.datum(randomize).call(chart.duration(1000)); // TODO automatic transition
-                                            });
-
-
-                                            function randomize(d) {
-                                                if (!d.randomizer) d.randomizer = randomizer(d);
-                                                d.ranges = d.ranges.map(d.randomizer);
-                                                d.markers = d.markers.map(d.randomizer);
-                                                d.measures = d.measures.map(d.randomizer);
-                                                return d;
-                                            }
-
-                                            function randomizer(d) {
-                                                var k = d3.max(d.ranges) * .2;
-                                                return function (d) {
-                                                    return Math.max(0, d + k * (Math.random() - .5));
-                                                };
-                                            }
-
-
                                         }
 
                                         break;
                                     }
                                 }
                             });
-
-
                         });
-
-
-
-
-
-
                 }]
         });
     }];
