5a93f2d599d01228dd92f92bb1429468e544c0e1
timhuebener <tim.huebener@gmail.com>
2018-05-28 14:45:09 +0200

    save

--- src/main/resources/plugin-webapp/centaur/app/diagram/counter/main.js
+++ src/main/resources/plugin-webapp/centaur/app/diagram/counter/main.js
@@ -52,8 +52,8 @@ define(['angular'], function(angular) {
 
             overlays.add(elementId, {
               position: {
-                top: 40,
-                left: 40
+                top: 80,
+                left: 50
               },
               show: {
                 minZoom: -Infinity,
@@ -92,12 +92,12 @@ define(['angular'], function(angular) {
            * Angular http.get promises that wait for a JSON object of
            * the process activity and the instance start time.
            */
-          //TODO: Get data from database
           $scope.executionSequenceCounter_temp = $http.get(Uri.appUri("plugin://centaur/:engine/execution-sequence-counter"), {
             catch: false
           });
-          // $scope.processActivityStatistics_temp = $http.get(Uri.appUri("plugin://centaur/:engine/process-activity?" + "procDefId=" + procDefId), {catch: false});
-          // $scope.instanceStartTime_temp = $http.get(Uri.appUri("plugin://centaur/:engine/instance-start-time"), {catch: false});
+          $scope.processVariables_temp = $http.get(Uri.appUri("plugin://centaur/:engine/process-variables" + "?procDefId=" + procDefId), {
+            catch: false
+          });
 
           /**
            * Waits until data is received from http.get request and
@@ -110,6 +110,8 @@ define(['angular'], function(angular) {
            */
           $q.all([$scope.executionSequenceCounter_temp]).then(function(data) {
             $scope.executionSequenceCounter = data[0]; //$scope.processActivityStatistics.data to access array with data from JSON object
+            $scope.processVariables = data[1];
+            console.log($scope.processVariables);
             /**
              * Extracts data from JSON objects and calls composeHTML()
              * function to add the extracted to the diagram.
