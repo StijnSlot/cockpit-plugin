ab7f54f15fe5763fd100ccaff0fde6694016fb09
Lukas Ant <lukas.ant@hotmail.com>
2018-05-29 10:43:54 +0200

    Added contracts

--- src/main/resources/plugin-webapp/centaur/app/diagram/bulletgraph/main.js
+++ src/main/resources/plugin-webapp/centaur/app/diagram/bulletgraph/main.js
@@ -293,12 +293,12 @@ define(['angular'], function (angular) {
 
                     /**
                      * Adds text to specified diagram element.
-                     * @param   Number  elementId   ID of diagram element
+                     * @param   String  elementId   ID of diagram element
                      * @param   Number  text        The text to be displayed
                      * @param   Object  shape       Shape of the element
                      * TODO: make into service
                      */
-                    function addTextToId(elementId, text, shape) {
+                    function addHTMLToId(elementId, text, shape) {
                         var $overlayHtml =
                             $(text)
                                 .css({
@@ -308,7 +308,7 @@ define(['angular'], function (angular) {
 
                         overlays.add(elementId, {
                             position: {
-                                top: -30,
+                                top: -40,
                                 left: 30
                             },
                             show: {
@@ -326,7 +326,7 @@ define(['angular'], function (angular) {
                      * process. So we calculate the current duration of each process.
                      *
                      * @param   Number  instance    Instance of a process
-                     * @param   Number  elementId   ID of diagram element that represents instance
+                     * @param   String  elementId   ID of diagram element that represents instance
                      */
                     function calculateCurDuration(instance, elementID) {
                         for (var j = 0; j < instance.length; j++) {
@@ -342,90 +342,137 @@ define(['angular'], function (angular) {
                     }
 
                     /**
-                     * Decides which time interval to use.
+                     * Decides which time unit to use.
                      *
                      * The database keeps track of the duration in milli seconds.
-                     * This is difficult to read in the diagram, so we convert the
-                     * milli senconds into following intervals: seconds, minutes,
-                     * hours, days, weeks to make it easier to read.
+                     * Since a discision has to be made to show in the bulletgraph,
+                     * this determines which time unit (seconds, minutes,
+                     * hours, days, weeks) should be used.
                      *
-                     * @param   Number  duration      duration of process
-                     * @return  String  durationHTML  duration as String
+                     * @param   Number  time      duration of process
+                     * @return  String            time unit choice
                      */
-                    function checkTimeStandard(duration) {
-                        if (duration > 1000 && duration < 60001) {
+                    function checkTimeUnit(time) {
+                        if (time > 1000 && time < 60001) {
                             return 'seconds';
-                        } else if (duration > 60000 && duration < 1440001) {
+                        } else if (time > 60000 && time < 1440001) {
                             return 'minutes';
-                        } else if (duration > 1440000 && duration < 34560001) {
+                        } else if (time > 1440000 && time < 34560001) {
                             return 'hours';
-                        } else if (duration > 34560000 && duration < 241920001) {
+                        } else if (time > 34560000 && time < 241920001) {
                             return 'days';
-                        } else if (duration > 241920000) {
+                        } else if (time > 241920000) {
                             return 'weeks';
                         } else {
                             return 'ms';
                         }
-                        return durationHTML;
-                    }
-
-                    function convertTimes(duration, choice) {
-                        if (choice == 'seconds') {
-                            var durationHTML = (Math.round(duration / 1000 * 10) / 10);
-                        } else if (choice == 'minutes') {
-                            var durationHTML = (Math.round(duration / 6000 * 10) / 10);
-                        } else if (choice == 'hours') {
-                            var durationHTML = (Math.round(duration / 1440000 * 10) / 10);
-                        } else if (choice == 'days') {
-                            var durationHTML = (Math.round(duration / 34560000 * 10) / 10);
-                        } else if (choice == 'weeks') {
-                            var durationHTML = (Math.round(duration / 241920000 * 10) / 10);
-                        } else {
-                            var durationHTML = duration;
-                        }
-                        return durationHTML;
                     }
 
                     /**
-                     * Combines all information of given process into single
-                     * String variable which is added to its diagram element.
+                     * Convert the duration into the chosen time unit.
+                     *
+                     * The database keeps track of the duration in milli seconds.
+                     * This is difficult to read in the diagram, so we convert the
+                     * milli senconds into following intervals: seconds, minutes,
+                     * hours, days, weeks to make it easier to read. The choice
+                     * determines which time unit to use.
+                     *
+                     * @param   Number  duration      duration of process
+                     * @param   String  choice        choice of time unit
+                     * @return  Number                duration as Integer
+                     */
+                    function convertTimes(duration, choice) {
+                        if (choice == 'seconds') {
+                            return (Math.round(duration / 1000 * 10) / 10);
+                        } else if (choice == 'minutes') {
+                            return (Math.round(duration / 6000 * 10) / 10);
+                        } else if (choice == 'hours') {
+                            return (Math.round(duration / 1440000 * 10) / 10);
+                        } else if (choice == 'days') {
+                            return (Math.round(duration / 34560000 * 10) / 10);
+                        } else if (choice == 'weeks') {
+                            return (Math.round(duration / 241920000 * 10) / 10);
+                        } else {
+                            return duration;
+                        }
+                    }
+
+                    /**
+                     * Combines all information of given process into a HTML line
+                     * which will be added to the HTML file. It will also set the graphsettings
+                     * which will be written to the class which is created in the HTML line.
                      *
                      * This function receives all duration information about a given process.
-                     * If any of duration variables are NULL it does not create
-                     * a hmtlText variable since there is nothing to display.
-                     * Otherwise it checks which time intervall to use for each
-                     * duration variable and combines them into one String variable, htmlText.
-                     * The htmlText variable is passed to the addTextToId() function
-                     * so that the duration varables are displayed next to the
-                     * process diagram element.
+                     * If any conditions which are defined in a seperate checkConditions() function
+                     * isn't satisfied, the bulletgraph will not display.
+                     * An HTML line will be passed to the addTextToId() function 
+                     * so that an class which includes an elementID will be added to the HTML file.
+                     * Information which has has been collected will be passed to the setGraphSettings()
+                     * function. Here it will create a bullet graph on he HTML line which has been
+                     * passed into the HTML file previously.
                      *
                      * @param   Number  minDuration   minimal duration of process
                      * @param   Number  avgDuration   average duration of process
                      * @param   Number  maxDuration   maximal duration of process
                      * @param   Number  curDuration   current duration of process
-                     * @param   Number  elementID     ID of element
+                     * @param   String  elementID     ID of element
                      * @param   Object  shape         Shape of the element
                      */
-                    function composeHTML(minDuration, avgDuration, maxDuration, curDuration, elementID, shape) {
-                        if (avgDuration != null && minDuration != null && maxDuration != null && avgDuration != '0') {
-                            var timeChoice = checkTimeStandard(maxDuration);
+                    function combineBulletgraphElements(minDuration, avgDuration, maxDuration, curDuration, elementID, shape) {
+                        if (checkConditions(minDuration, avgDuration, maxDuration, curDuration)) {
+                            var timeChoice = checkTimeUnit(maxDuration);
                             var minDuration = convertTimes(minDuration, timeChoice);
                             var avgDuration = convertTimes(avgDuration, timeChoice);
                             var maxDuration = convertTimes(maxDuration, timeChoice);
-                            if (curDuration != null) {
-                                var curDuration = convertTimes(curDuration, timeChoice);
-                                var htmlText = '<div class="bullet-duration-' + elementID + '"> </div>';
-                                var colorBullet = determineColor(avgDuration, maxDuration, curDuration);
-                                addTextToId(elementID, htmlText, shape);
-                                setGraphSettings(elementID, maxDuration, checkIfCurBiggerMax(curDuration, maxDuration), avgDuration, colorBullet);
-                            } else {
-                                var curDuration = 0;
-                            }
-
-
+                            var curDuration = convertTimes(curDuration, timeChoice);
+                            var colorBullet = determineColor(avgDuration, maxDuration, curDuration);
+                            addHTMLToId(elementID, createHTML(elementID), shape);
+                            setGraphSettings(elementID, maxDuration, checkIfCurBiggerMax(curDuration, maxDuration), avgDuration, colorBullet);
                         }
                     }
 
+                    /**
+                     * This function will check if the conditions to show the bulletgraph are
+                     * satisfied.
+                     *
+                     * The conditions to show the bulletgraph are satisfied if:
+                     * - Any of the duration variables are not equal to NULL
+                     * - The average duration is not equal to '0'.
+                     *
+                     * @param   Number  minDuration   minimal duration of process
+                     * @param   Number  avgDuration   average duration of process
+                     * @param   Number  maxDuration   maximal duration of process
+                     * @param   Number  curDuration   current duration of process
+                     * @return  Boolean               if condtions are satisfied or not
+                     */
+                    function checkConditions(minDuration, avgDuration, maxDuration, curDuration) {
+                        if (avgDuration != null && minDuration != null && maxDuration != null  && curDuration != null && avgDuration != '0') {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+
+                    /**
+                     * Creates an HTML line with has a class that includes the elementID
+                     * @param   String  elementID   Variable to be converted.
+                     * @return  String              A string which represents an HTML line which will be added later
+                     */
+                    function createHTML(elementID) {
+                        return '<div class="bullet-duration-' + elementID + '"> </div>';
+                    }
+
+                    /**
+                     * This function decides which color the bullet graph should have on the following
+                     * conditions which are specified in the URD:
+                     * - Green: If the current duration is less or equal to the average and maximal duration
+                     * - Orange: If the current duration is less or equal to the maximal duration and greater than the average duration
+                     * - Red: If he current duration is greater than both the average durationa and the maximal duration
+                     * @param   Number  avgDuration   average duration of process
+                     * @param   Number  maxDuration   maximal duration of process
+                     * @param   Number  curDuration   current duration of process
+                     * @return  String              A string which represents the color
+                     */
                     function determineColor(avgDuration, maxDuration, curDuration) {
                         if (curDuration <= maxDuration && curDuration <= avgDuration) {
                             return 'green';
@@ -436,6 +483,13 @@ define(['angular'], function (angular) {
                         }
                     }
 
+                    /**
+                     * This function checks if the current duration is greater or equal to the maximal duration
+                     * since the bullet graph should not exceed the maximal duration.
+                     * @param   Number  maxDuration   maximal duration of process
+                     * @param   Number  curDuration   current duration of process
+                     * @return  Number                either current duration or maximal duration
+                     */
                     function checkIfCurBiggerMax(curDuration, maxDuration) {
                         if (curDuration >= maxDuration) {
                             return maxDuration;
@@ -444,6 +498,23 @@ define(['angular'], function (angular) {
                         }
                     }
 
+                    /**
+                     * This function combines all information passed into it to set the settings
+                     * of the bullet graph. This function is made from the code which is provided
+                     * on the following github: https://gist.github.com/mbostock/4061961#file-bullet-js (accesed on 29-5-2018).
+                     * The functions which we included are coming also from this github repository.
+                     * Additionally we are also using the D3 library (https://d3js.org/).
+                     *
+                     * This function adds the bulletgraph to the html class which is defined in a seperate function. 
+                     * The same function will be selected by using the elementID. In the data variable, the data for
+                     * the bulletgraph will be set. This data includes the range, the current value and the marker value.
+                     *
+                     * @param   String  elementID     ID of element
+                     * @param   Number  rangeBullet   range of bulletgraph
+                     * @param   Number  currentBullet current value of bulletgraph
+                     * @param   Number  markerBullet  marker value of bulletgraph
+                     * @param   Number  colorBullet   color of bulletgraph
+                     */
                     function setGraphSettings(elementID, rangeBullet, currentBullet, markerBullet, colorBullet) {
                         var cssClass = '.bullet-duration-' + elementID;
                         var data = [
@@ -456,7 +527,7 @@ define(['angular'], function (angular) {
                         var container = d3.select(cssClass).node().getBoundingClientRect();
                         var margin = { top: 5, right: 5, bottom: 15, left: 5 },
                             width = 100 - margin.left - margin.right,
-                            height = 50 - margin.top - margin.bottom;
+                            height = 40 - margin.top - margin.bottom;
 
                         var chart = d3.bullet(width, height)
                             .width(width)
@@ -467,7 +538,7 @@ define(['angular'], function (angular) {
                             .enter().append("svg")
                             .attr("class", "bullet")
                             .attr("width", 100)
-                            .attr("height", 50)
+                            .attr("height", 40)
                             .append("g")
                             .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
                             .call(chart);
@@ -516,7 +587,7 @@ define(['angular'], function (angular) {
                                     var getMaxDuration = $scope.processActivityStatistics.data[i].maxDuration;
                                     var getCurDuration = calculateCurDuration($scope.instanceStartTime.data, element.id);
 
-                                    composeHTML(getMinDuration, getAvgDuration, getMaxDuration, getCurDuration, element.id, shape);
+                                    combineBulletgraphElements(getMinDuration, getAvgDuration, getMaxDuration, getCurDuration, element.id, shape);
                                     break;
                                 }
                             }
