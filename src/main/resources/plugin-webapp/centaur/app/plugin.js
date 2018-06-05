7e7f259c9f5e152069e1193066bb7ee4ee08e81c
Stijn Slot <stynder@live.nl>
2018-06-05 14:16:26 +0200

    Removed demotext

--- src/main/resources/plugin-webapp/centaur/app/plugin.js
+++ src/main/resources/plugin-webapp/centaur/app/plugin.js
@@ -7,8 +7,7 @@ define(function (require) {
     var angular = require('angular');
     var diagramModule = require('./diagram/main');
     var bottomTabsModule = require('./bottomTabs/main');
-    var demoText = require('./demoText/main');
-    return angular.module('cockpit.plugin.centaur', [diagramModule.name, demoText.name, bottomTabsModule.name]);
+    return angular.module('cockpit.plugin.centaur', [diagramModule.name, bottomTabsModule.name]);
 });
 
 //git hook test
