60b51814114cc36231ea7f81be6550d03ecb3dff
Lukas Ant <lukas.ant@hotmail.com>
2018-06-06 10:33:05 +0200

    Still one failing

--- src/test/js/processDefinition/duration/util.spec.js
+++ src/test/js/processDefinition/duration/util.spec.js
@@ -43,57 +43,6 @@ describe('calculate current duration test', function(){
 
 });
 
-describe('check times conversion test', function () {
-
-    var duration;
-    describe('check duration format is correct', function () {
-
-        it('test for duration to be in seconds', function () {
-            duration = 2000;
-            expect(util.checkTimes(duration)).to.eql('2 seconds');
-            duration = 60000;
-            expect(util.checkTimes(duration)).to.eql('60 seconds');
-            duration = 61000;
-            expect(util.checkTimes(duration)).to.not.eql('61 seconds');
-        });
-
-        it('test for duration to be in minutes', function(){
-            duration = 60001;
-            expect(util.checkTimes(duration)).to.eql('1 minutes');
-            duration = 1440000;
-            expect(util.checkTimes(duration)).to.eql('24 minutes');
-            duration = 3600001;
-            expect(util.checkTimes(duration)).to.not.eql('60 minutes');
-        });
-
-        it('test for duration to be in hours', function(){
-            duration = 3600001;
-            expect(util.checkTimes(duration)).to.eql('1 hours');
-            duration = 86400000;
-            expect(util.checkTimes(duration)).to.eql('24 hours');
-        });
-
-        it('test for duration to be in days', function(){
-            duration = 86400001;
-            expect(util.checkTimes(duration)).to.eql('1 days');
-            duration = 604800000;
-            expect(util.checkTimes(duration)).to.eql('7 days');
-        });
-
-        it('test for duration to be in weeks', function(){
-            duration = 604800001;
-            expect(util.checkTimes(duration)).to.eql('1 weeks');
-            duration = 604800001*2;
-            expect(util.checkTimes(duration)).to.eql('2 weeks');
-            duration = 604800001*5;
-            expect(util.checkTimes(duration)).to.eql('5 weeks');
-
-        });
-
-    });
-
-});
-
 
 describe('check Conditions', function () {
     var avgDuration, maxDuration;
