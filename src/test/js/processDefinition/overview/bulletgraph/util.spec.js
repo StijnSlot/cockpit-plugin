92d6cddffcaa293a7760063450cc951fc06f53bd
Lukas Ant <lukas.ant@hotmail.com>
2018-06-19 10:27:04 +0200

    Removed unnecessary testcases

--- src/test/js/processDefinition/overview/bulletgraph/util.spec.js
+++ src/test/js/processDefinition/overview/bulletgraph/util.spec.js
@@ -11,26 +11,4 @@ describe('bulletGraph process definition tests', function() {
     it('should find duration util file', function() {
         expect(util).to.exist;
     });
-
-// describe('calculate current duration test', function(){
-
-//     var instance;
-//     var elementID;
-//     describe('check if time difference is correct', function () {
-//         beforeEach(function () {
-//             instance = [{activityId: 12, startTime: 0}, {activityId:14, startTime: 2}];
-//         });
-
-//         it('test if the time difference is returned at all', function () {
-//             elementID = 14;
-//             expect(util.calculateCurDuration(instance, elementID)).to.be.a('number');
-//             elementID = 16;
-//             expect(util.calculateCurDuration(instance, elementID)).to.be.null;
-//         });
-
-//     });
-
-
-
-// });
 });
