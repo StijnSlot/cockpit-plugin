45a5b8f5c95fde216f9e5b54a0f8bd3d1147e207
Martin Cekodhima <mcekodhima@gmail.com>
2018-06-20 15:42:34 +0200

    now

--- src/main/java/org/camunda/bpm/cockpit/plugin/centaur/resources/CockpitPluginRootResource.java
+++ src/main/java/org/camunda/bpm/cockpit/plugin/centaur/resources/CockpitPluginRootResource.java
@@ -54,10 +54,24 @@ public class CockpitPluginRootResource extends AbstractCockpitPluginRootResource
      *
      * @param engineName The engine currently being used by the platform.
      * @param procDefId  The id of a process definition.
+     * @return List of statistics about the process definition with id = procDefId.
+     */
+    @Path("{engineName}/process-activity")
+    public ProcessActivityResource getProcessInstanceActivityResource(
+            @PathParam("engineName") String engineName,
+            @QueryParam("procDefId") String procDefId) {
+        return subResource(new ProcessActivityResource(engineName, procDefId), engineName);
+    }
+
+    /**
+     * Returns resource for path {engineName}/process-variables.
+     *
+     * @param engineName The engine currently being used by the platform.
+     * @param procDefId  The id of a process definition.
      * @param procInstId The id of a process instance.
      * @return List of all available variables for the instance.
      */
-    @Path("{engineName}/process-activity")
+    @Path("{engineName}/process-variables")
     public ProcessVariablesResource getProcessVariableResource(
             @PathParam("engineName") String engineName,
             @QueryParam("procDefId") String procDefId,
