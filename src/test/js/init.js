658dcfd173f70b3704deeb57eb78c1a746d2ceb3
Martin Cekodhima <mcekodhima@gmail.com>
2018-06-29 14:58:01 +0200

    added angular as a dev dependency

--- src/test/js/init.js
+++ src/test/js/init.js
@@ -1,8 +1,10 @@
 'use strict';
 
+import angular0 from "angular";
+
 global.sinon = require('sinon');
 global.chai = require('chai');
-global.angular.equals = sinon.stub();
+global.angular = angular0;
 global.expect = chai.expect;
 global.requirejs  = require('requirejs');
 requirejs.config({
